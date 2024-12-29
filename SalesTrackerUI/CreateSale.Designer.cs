fccd9cc9aaf5d87cf8084fac6b0a76fe4881e74d
Sruthi Dontu <sruthi99@live.unc.edu>
2024-12-29 03:24:02 -0500

    createsale files

---
+++ SalesTrackerUI/CreateSale.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace SalesTrackerUI
+{
+    partial class CreateSale
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // CreateSale
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "CreateSale";
+            Size = new Size(1493, 396);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
